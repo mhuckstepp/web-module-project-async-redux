c06483df877c1a364fceff1942eb5502b843dc68
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-18 07:52:49 -0700

    add more styling

--- comics-with-thunk/src/components/FullComic.js
+++ comics-with-thunk/src/components/FullComic.js
@@ -21,16 +21,21 @@ const FullComic = () => {
         }
       });
     } else {
-      axios.get(`http://xkcd.com/${num}/info.0.json`).then((res) => {
-        let resultObj = {
-          alt: res.data.alt,
-          img: res.data.img,
-          num: res.data.num,
-          title: res.data.title,
-          comments: [],
-        };
-        setSelectComic(resultObj);
-      });
+      axios
+        .get(`http://xkcd.com/${num}/info.0.json`)
+        .then((res) => {
+          let resultObj = {
+            alt: res.data.alt,
+            img: res.data.img,
+            num: res.data.num,
+            title: res.data.title,
+            comments: [],
+          };
+          setSelectComic(resultObj);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }, [num, comics]);
 
@@ -47,51 +52,54 @@ const FullComic = () => {
 
   return (
     <div>
-      <h1>{selectComic.title}</h1>
-      <img
-        style={{ maxHeight: "550px" }}
-        src={selectComic.img}
-        alt={selectComic.alt}
-      />
-      <p>{selectComic.alt}</p>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "30%",
-          margin: "0 auto",
-        }}
-        class="columns"
-      >
-        <textarea
-          class="textarea is-small"
-          style={{
-            width: "30%",
-            margin: "30px auto 10px",
-            textAlign: "center",
-          }}
-          name="comment"
-          placeholder="add your own caption"
-          type="text"
-          onChange={handleChange}
-          value={comment}
-        ></textarea>
-
-        <button
-          class="button is-small is-info"
-          style={{
-            width: "30%",
-          }}
-          type="submit"
-          onClick={() => handleSubmit(comment, num)}
-        >
-          Add your own caption!
-        </button>
+      <div className="container">
+        <h1>{selectComic.title}</h1>
+        <img
+          style={{ maxHeight: "550px" }}
+          src={selectComic.img}
+          alt={selectComic.alt}
+        />
+        <p>{selectComic.alt}</p>
         {selectComic.comments &&
           selectComic.comments.map((comment) => (
             <div class="media"> {comment} </div>
           ))}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "30%",
+          }}
+          class="columns"
+        >
+          <textarea
+            class="textarea is-small"
+            style={{
+              width: "30%",
+              margin: "30px auto 10px",
+              textAlign: "center",
+            }}
+            name="comment"
+            placeholder="add your own caption"
+            type="text"
+            onChange={handleChange}
+            value={comment}
+          ></textarea>
+
+          <button
+            class="button is-small is-info"
+            style={{
+              width: "40%",
+              padding: "2%",
+              margin: "10px",
+            }}
+            type="submit"
+            onClick={() => handleSubmit(comment, num)}
+          >
+            Add your own caption!
+          </button>
+        </div>
       </div>
     </div>
   );
