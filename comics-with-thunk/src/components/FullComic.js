4a2ec553cfae1a03bdb517c3e8dff15bac3e1e8a
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-23 08:52:53 -0700

    buttons next and previous and jump to form

--- comics-with-thunk/src/components/FullComic.js
+++ comics-with-thunk/src/components/FullComic.js
@@ -2,26 +2,37 @@ import React from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import { addComment } from "../actions";
+// import { addComment } from "../actions";
 import axios from "axios";
 
 const FullComic = () => {
   let { num } = useParams();
+  num = Number(num);
   const comics = useSelector((state) => state.comics);
   const [selectComic, setSelectComic] = useState([{}]);
-  const [comment, setComment] = useState("");
-  const dispatch = useDispatch();
   let history = useHistory();
+  // const [comment, setComment] = useState("");
+  // const dispatch = useDispatch();
 
-  console.log(history.location);
+  const handleNext = () => {
+    console.log(`/${num + 1}`);
+    history.push(`/${num + 1}`);
+  };
+
+  const handleLast = () => {
+    history.push(`/${num - 1}`);
+  };
 
   useEffect(() => {
-    if (comics.length > 0) {
+    // Checks comics array is populated and includes the number we are looking for.
+    if (comics.length > 0 && comics.map((comic) => comic.num).includes(num)) {
+      console.log(num);
       comics.forEach((comic) => {
         if (comic.num == num) {
           setSelectComic(comic);
         }
       });
+      // if we dont find existing array we go and 'get' it
     } else {
       axios
         .get(`http://xkcd.com/${num}/info.0.json`)
@@ -39,7 +50,7 @@ const FullComic = () => {
           console.log(err);
         });
     }
-  }, [num, comics, history.location.pathname]);
+  }, [num, comics]);
 
   // const handleChange = (e) => {
   //   setComment(e.target.value);
@@ -55,10 +66,29 @@ const FullComic = () => {
   return (
     <div>
       <div className="flex flex-col items-center w-full ">
+        <div className="flex">
+          <button
+            className="bg-gradient-to-r mr-12 p-4 from-blue-500 rounded font-bold"
+            onClick={() => handleLast()}
+          >
+            Previous
+          </button>
+          <button
+            className="bg-gradient-to-l ml-6 p-4 from-blue-500 rounded font-bold"
+            onClick={() => handleNext()}
+          >
+            Next
+          </button>
+        </div>
         <h1 className="text-7xl mb-8">{selectComic.title}</h1>
-        <img className="w-10/12" src={selectComic.img} alt={selectComic.alt} />
-        <p className="text-4xl my-16 w-9/12">{selectComic.alt}</p>
-        <p className="text-l mb-4"># {selectComic.num}</p>
+        <img
+          className="max-w-screen-xl max-h-screen"
+          src={selectComic.img}
+          alt={selectComic.alt}
+        />
+        <p className="text-2xl mt-16 w-9/12">{selectComic.alt}</p>
+        <p className="text-l my-4"># {selectComic.num}</p>
+
         {/* {selectComic.comments &&
           selectComic.comments.map((comment) => (
             <div class="media"> {comment} </div>
